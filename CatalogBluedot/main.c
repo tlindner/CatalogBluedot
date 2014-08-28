871e8fed89517bb4746b831d918084d9aed7b35e
Tim <tim@Tims-iMac.local>
2014-08-27 18:07:33 -0700

    Initial Commit

---
+++ CatalogBluedot/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CatalogBluedot
+//
+//  Created by Tim on 8/27/14.
+//  Copyright (c) 2014 Tim. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
